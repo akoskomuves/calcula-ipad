54bf202f97a67623bd62237f0411157655df0381
Akos Komuves <komuvesakos@icloud.com>
2019-07-23 11:17:22 +0200

    fix: picture buttons

--- calculator/calculator/CustomButton.swift
+++ calculator/calculator/CustomButton.swift
@@ -5,25 +5,68 @@ class CustomButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
     }
 
-    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-        setup()
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    required init?(coder decoder: NSCoder) {
-        super.init(coder: decoder)
-        setup()
-    }
-
-    func setup() {
-        self.layer.cornerRadius = 0.5 * self.bounds.size.height
-        self.clipsToBounds = true
-    }
-    
     override var isHighlighted: Bool {
         didSet { if isHighlighted { isHighlighted = false } }
     }
 
+    var tappedImage: UIImage?
+    var tappedSmallImage: UIImage?
+    var selectedImage: UIImage?
+    var selectedSmallImage: UIImage?
+    var normalImage: UIImage?
+    var normalSmallImage: UIImage?
+
+    func setImages(tappedImage: UIImage?,
+                   tappedSmallImage: UIImage?,
+                   selectedImage: UIImage?,
+                   selectedSmallImage: UIImage?,
+                   normalImage: UIImage?,
+                   normalSmallImage: UIImage?) {
+        self.tappedImage = tappedImage
+        self.tappedSmallImage = tappedSmallImage
+        self.selectedImage = selectedImage
+        self.selectedSmallImage = selectedSmallImage
+        self.normalImage = normalImage
+        self.normalSmallImage = normalSmallImage
+    }
+
+    var buttonState = ButtonState.normal {
+        didSet {
+            switch buttonState {
+            case .tapped:
+                switch screenSize {
+                case .normal:
+                    self.setBackgroundImage(tappedImage, for: .normal)
+                case .small:
+                    self.setBackgroundImage(tappedSmallImage, for: .normal)
+                }
+            case .normal:
+                switch screenSize {
+                case .normal:
+                    self.setBackgroundImage(normalImage, for: .normal)
+                case .small:
+                    self.setBackgroundImage(normalSmallImage, for: .normal)
+                }
+            case .selected:
+                switch screenSize {
+                case .normal:
+                    self.setBackgroundImage(selectedImage, for: .normal)
+                case .small:
+                    self.setBackgroundImage(selectedSmallImage, for: .normal)
+                }
+            }
+        }
+    }
+
+    enum ButtonState {
+        case tapped
+        case normal
+        case selected
+    }
 }
--- calculator/calculator/ViewController.swift
+++ calculator/calculator/ViewController.swift
@@ -46,6 +46,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let image = UIImage(named: "ac_tap")
+        let imageView = UIImageView(image: image)
+        self.view.addSubview(imageView)
+
+//        acButton.setImages(tappedImage: image,
+//                           tappedSmallImage: UIImage(named: "ac_tap_small"),
+//                           selectedImage: nil, selectedSmallImage: nil,
+//                           normalImage: UIImage(named: "ac_norm"),
+//                           normalSmallImage: UIImage(named: "ac_small"))
+//        reverseButton.setImages(tappedImage: UIImage(named: "plus_minus_tap"), tappedSmallImage: UIImage(named: "plus_minus_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "plus_minus_norm"), normalSmallImage: UIImage(named: "plus_minus_small"))
+//        percentageButton.setImages(tappedImage: UIImage(named: "perc_tap"), tappedSmallImage: UIImage(named: "perc_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "perc_norm"), normalSmallImage: UIImage(named: "perc_small"))
+//        divideButton.setImages(tappedImage: UIImage(named: "div_tap"), tappedSmallImage: UIImage(named: "div_tap_small"), selectedImage: UIImage(named: "div_sel"), selectedSmallImage: UIImage(named: "div_sel_small"), normalImage: UIImage(named: "div_norm"), normalSmallImage: UIImage(named: "div_small"))
+//        sevenButton.setImages(tappedImage: UIImage(named: "7_tap"), tappedSmallImage: UIImage(named: "7_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "7_norm"), normalSmallImage: UIImage(named: "7_small"))
+//        eightButton.setImages(tappedImage: UIImage(named: "8_tap"), tappedSmallImage: UIImage(named: "8_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "8_norm"), normalSmallImage: UIImage(named: "8_small"))
+//        nineButton.setImages(tappedImage: UIImage(named: "9_tap"), tappedSmallImage: UIImage(named: "9_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "9_norm"), normalSmallImage: UIImage(named: "9_small"))
+//        sixthButton.setImages(tappedImage: UIImage(named: "6_tap"), tappedSmallImage: UIImage(named: "6_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "6_norm"), normalSmallImage: UIImage(named: "6_small"))
+//        fifthButton.setImages(tappedImage: UIImage(named: "5_tap"), tappedSmallImage: UIImage(named: "5_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "5_norm"), normalSmallImage: UIImage(named: "5_small"))
+//        fourthButton.setImages(tappedImage: UIImage(named: "4_tap"), tappedSmallImage: UIImage(named: "4_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "4_norm"), normalSmallImage: UIImage(named: "4_small"))
+//        thirdButton.setImages(tappedImage: UIImage(named: "3_tap"), tappedSmallImage: UIImage(named: "3_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "3_norm"), normalSmallImage: UIImage(named: "3_small"))
+//        secondButton.setImages(tappedImage: UIImage(named: "2_tap"), tappedSmallImage: UIImage(named: "2_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "2_norm"), normalSmallImage: UIImage(named: "2_small"))
+//        firstButton.setImages(tappedImage: UIImage(named: "1_tap"), tappedSmallImage: UIImage(named: "1_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "1_norm"), normalSmallImage: UIImage(named: "1_small"))
+//        zeroButton.setImages(tappedImage: UIImage(named: "0_tap"), tappedSmallImage: UIImage(named: "0_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "0_norm"), normalSmallImage: UIImage(named: "0_small"))
+//        multiplyButton.setImages(tappedImage: UIImage(named: "mult_tap"), tappedSmallImage: UIImage(named: "mult_tap_small"), selectedImage: UIImage(named: "mult_sel"), selectedSmallImage: UIImage(named: "mult_sel_small"), normalImage: UIImage(named: "mult_norm"), normalSmallImage: UIImage(named: "mult_small"))
+//        substractionButton.setImages(tappedImage: UIImage(named: "sub_tap"), tappedSmallImage: UIImage(named: "sub_tap_small"), selectedImage: UIImage(named: "sub_sel"), selectedSmallImage: UIImage(named: "sub_sel_small"), normalImage: UIImage(named: "sub_norm"), normalSmallImage: UIImage(named: "sub_small"))
+//        additionButton.setImages(tappedImage: UIImage(named: "add_tap"), tappedSmallImage: UIImage(named: "add_tap_small"), selectedImage: UIImage(named: "add_sel"), selectedSmallImage: UIImage(named: "add_sel_small"), normalImage: UIImage(named: "add_norm"), normalSmallImage: UIImage(named: "add_small"))
+//        dotButton.setImages(tappedImage: UIImage(named: "dot_tap"), tappedSmallImage: UIImage(named: "dot_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "dot_norm"), normalSmallImage: UIImage(named: "dot_small"))
+//        equalButton.setImages(tappedImage: UIImage(named: "equal_tap"), tappedSmallImage: UIImage(named: "equal_tap_small"), selectedImage: nil, selectedSmallImage: nil, normalImage: UIImage(named: "equal_norm"), normalSmallImage: UIImage(named: "equal_small"))
+
         buttons.append(acButton)
         buttons.append(reverseButton)
         buttons.append(percentageButton)
@@ -72,20 +100,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func numberDidTap(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 38/255, green: 38/255, blue: 38/255, alpha: 1)
+        if let button = sender as? CustomButton {
+            button.buttonState = .tapped
+        }
         if operation {
-            self.highlightedButton?.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
-
-            if let highlightedButton = highlightedButton {
-                switch op {
-                case "*": highlightedButton.setImage(UIImage(named: "Multiplication"), for: .normal)
-                case "/": highlightedButton.setImage(UIImage(named: "Division"), for: .normal)
-                case "-": highlightedButton.setImage(UIImage(named: "Substraction"), for: .normal)
-                case "+": highlightedButton.setImage(UIImage(named: "Addition"), for: .normal)
-                default: ()
-                }
-
-            }
+            self.highlightedButton?.buttonState = .normal
         }
         if let number = sender.accessibilityIdentifier {
             if number == "." {
@@ -94,7 +113,7 @@ class ViewController: UIViewController {
                 }
                 hasComma = true
             }
-            var textnum = String(numberlabel.text!)
+            var textnum = String(numberlabel.accessibilityIdentifier ?? "")
             if operation {
                 textnum = (number == ".") ? "0" : ""
                 operation = false
@@ -106,7 +125,9 @@ class ViewController: UIViewController {
 
     //clear button action
     @IBAction func acaction(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 109/255, green: 109/255, blue: 109/255, alpha: 1)
+        if let acButton = sender as? CustomButton {
+            acButton.buttonState = .normal
+        }
         numberlabel.text = "0"
         operation = true
         hasComma = false
@@ -114,7 +135,9 @@ class ViewController: UIViewController {
 
     //number making negitive
     @IBAction func some(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 109/255, green: 109/255, blue: 109/255, alpha: 1)
+        if let negative = sender as? CustomButton {
+            negative.buttonState = .normal
+        }
         var textnum = String(numberlabel.text!)
         textnum = "-" + textnum
         numberlabel.text = textnum
@@ -123,8 +146,9 @@ class ViewController: UIViewController {
 
     //percent button
     @IBAction func percentage(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 109/255, green: 109/255, blue: 109/255, alpha: 1)
-
+        if let perc = sender as? CustomButton {
+            perc.buttonState = .normal
+        }
         var number1 = numberlabel.text!.doubleValue
         number1 = number1/100.0
         numberlabel.text = String(number1)
@@ -134,30 +158,19 @@ class ViewController: UIViewController {
     
     @IBAction func baseOperatorDidTap(_ sender: UIButton) {
         if operation {
-                self.highlightedButton?.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
-
-                if let highlightedButton = highlightedButton {
-                switch op {
-                case "*": highlightedButton.setImage(UIImage(named: "Multiplication"), for: .normal)
-                case "/": highlightedButton.setImage(UIImage(named: "Division"), for: .normal)
-                case "-": highlightedButton.setImage(UIImage(named: "Substraction"), for: .normal)
-                case "+": highlightedButton.setImage(UIImage(named: "Addition"), for: .normal)
-                default: ()
-                }
-
-            }
+                self.highlightedButton?.buttonState = .normal
         }
         highlightedButton = sender as? CustomButton
         sender.backgroundColor = .white
         op = sender.accessibilityIdentifier ?? ""
 
-        switch op {
-        case "*": sender.setImage(UIImage(named: "multiplication_selected"), for: .normal)
-        case "/": sender.setImage(UIImage(named: "division_selected"), for: .normal)
-        case "-": sender.setImage(UIImage(named: "substraction_selected"), for: .normal)
-        case "+": sender.setImage(UIImage(named: "addition_selected"), for: .normal)
-        default: ()
-        }
+//        switch op {
+//        case "*": sender.setImage(UIImage(named: "multiplication_selected"), for: .normal)
+//        case "/": sender.setImage(UIImage(named: "division_selected"), for: .normal)
+//        case "-": sender.setImage(UIImage(named: "substraction_selected"), for: .normal)
+//        case "+": sender.setImage(UIImage(named: "addition_selected"), for: .normal)
+//        default: ()
+//        }
 
         number1 = numberlabel.text!.doubleValue
         operation = true
@@ -165,8 +178,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func eqauls(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
-        highlightedButton?.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
+//        sender.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
+//        highlightedButton?.backgroundColor = UIColor(red: 255/255, green: 123/255, blue: 0/255, alpha: 1)
+
+        if let eq = sender as? CustomButton {
+            eq.buttonState = .normal
+        }
+        self.highlightedButton?.buttonState = .normal
 
         let number2 = numberlabel.text!.doubleValue
         var result: Double?
@@ -184,13 +202,13 @@ class ViewController: UIViewController {
         }
 
         if let highlightedButton = highlightedButton {
-            switch op {
-            case "*": highlightedButton.setImage(UIImage(named: "Multiplication"), for: .normal)
-            case "/": highlightedButton.setImage(UIImage(named: "Division"), for: .normal)
-            case "-": highlightedButton.setImage(UIImage(named: "Substraction"), for: .normal)
-            case "+": highlightedButton.setImage(UIImage(named: "Addition"), for: .normal)
-            default: ()
-            }
+//            switch op {
+//            case "*": highlightedButton.setImage(UIImage(named: "Multiplication"), for: .normal)
+//            case "/": highlightedButton.setImage(UIImage(named: "Division"), for: .normal)
+//            case "-": highlightedButton.setImage(UIImage(named: "Substraction"), for: .normal)
+//            case "+": highlightedButton.setImage(UIImage(named: "Addition"), for: .normal)
+//            default: ()
+//            }
         }
         if let result = result {
             let value  = String(format: "%g", result)
@@ -204,33 +222,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func changeNumberColorTouchDown(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 83/255, green: 83/255, blue: 83/255, alpha: 1)
+        if let button = sender as? CustomButton {
+            button.buttonState = .tapped
+        }
     }
 
     @IBAction func changeOperatorColorTouchDown(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 255/255, green: 189/255, blue: 96/255, alpha: 1)
+        if let button = sender as? CustomButton {
+            button.buttonState = .tapped
+        }
     }
 
     @IBAction func changeUnaryColorTouchDown(_ sender: UIButton) {
-        sender.backgroundColor = UIColor(red: 163/255, green: 163/255, blue: 163/255, alpha: 1)
+        if let button = sender as? CustomButton {
+            button.buttonState = .tapped
+        }
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         print(size.width)
 
         if size.width <= 320 {
-            numberlabel.font = UIFont.systemFont(ofSize: 46, weight: .light)
-            labelStackViewSpace.constant = 15
-            numberLabelHeight.constant = 60
-            self.stackViewHeight.constant = 360
-            self.allHeights.forEach({ height in
-                height.constant = 60
-            })
-            buttons.forEach({ button in
-                button.layer.cornerRadius = 0.5 * 60
-                button.layoutIfNeeded()
-                button.setNeedsUpdateConstraints()
-            })
+            screenSize = .normal
 
             stackView.spacing = 15
             firstStackView.spacing = 15
@@ -238,18 +251,8 @@ class ViewController: UIViewController {
             thirdStackView.spacing = 15
             fourthStackView.spacing = 15
         } else {
-            numberlabel.font = UIFont.systemFont(ofSize: 64, weight: .light)
-            labelStackViewSpace.constant = 20
-            numberLabelHeight.constant = 80
-            self.stackViewHeight.constant = 480
-            self.allHeights.forEach({ height in
-                height.constant = 80
-            })
-            buttons.forEach({ button in
-                button.layer.cornerRadius = 0.5 * 80
-                button.layoutIfNeeded()
-                button.setNeedsUpdateConstraints()
-            })
+
+            screenSize = .small
 
             stackView.spacing = 20
             firstStackView.spacing = 20
@@ -257,6 +260,10 @@ class ViewController: UIViewController {
             thirdStackView.spacing = 20
             fourthStackView.spacing = 20
         }
+
+        buttons.forEach({ button in
+            button.buttonState = button.buttonState
+        })
     }
 
 }
@@ -275,4 +282,11 @@ extension String {
         }
         return 0
     }
+}
+
+public var screenSize = ScreenSize.normal
+
+public enum ScreenSize {
+    case small
+    case normal
 }
